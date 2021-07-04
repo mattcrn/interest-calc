d324f9afb23b8385c9eab69cdb271f9c02de7398
Matthias Frank <matthias.frank@towa.at>
2021-07-04 18:32:55 +0200

    feat: moved to a json based interest rate

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,96 @@
 <template>
   <div class="App">
-    <header class="App-header">
-      <img src="/logo.svg" class="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.vue</code> and save to reload.
-      </p>
-      <a
-        class="App-link"
-        href="https://vuejs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >{{ message }}</a>
-    </header>
+    <label for="amount">Kreditsumme in €</label>
+    <input v-model="amount" name="amount" type="text">
+    <label for="duration">Laufzeit in Jahren</label>
+    <input v-model="duration" name="duration" type="text">
+    <label for="interest">Zins pa</label>
+    <input v-model="interest" name="interest" type="text">
+    <button @click="calculate">Berechnen</button>
+<table style="width:50%">
+  <tr>
+    <th>Jahr</th>
+    <th>Rückzahlen</th>
+    <th>Zins</th>
+    <th>Restbetrag</th>
+    <th>Monatliche Rate</th>
+  </tr>
+  <tr v-for="(result, index) in results" :key="`tr-${index}`">
+    <td>{{ index + 1 }}</td>
+    <td>{{ result.repayment.toFixed(2) }}</td>
+    <td>{{ result.yearlyInterest.toFixed(2) }}</td>
+    <td>{{ result.remaining.toFixed(2) }}</td>
+    <td>{{ (result.annuity / 12).toFixed(2) }}</td>
+  </tr>
+</table>
+<line-chart :chartdata="interestData"/>
   </div>
 </template>
 
 <script>
+import interestData from './interestData'
+// import LineChart from './components/LineChart.vue'
+// import Test from './components/Test.vue'
+
 export default {
+  components: {
+  },
   data() {
     return {
-      message: "Learn Vue"
+      amount: 640000,
+      duration: 35,
+      interest: 0.3,
+      results: []
     };
+  },
+  computed: {
+    interestTimeData() {
+      const interestPerYear = [];
+      interestData.forEach((data) => {
+        console.log(data);
+        for (let i = 0; i < data.duration; i++) {
+          interestPerYear.push(data.interest); 
+        }
+      })
+      console.log(interestPerYear);
+      return interestPerYear;
+    }
+  },
+  methods: {
+    calculate() {
+      console.log(this.interestTimeData[0]);
+      this.results = []
+      let remaining = this.amount;
+      let annuity = this.calculateAnnuity(this.amount, this.duration, this.interestTimeData[0]);
+      for (let i = 0; i < this.duration; i++) {
+        const interest = this.interestTimeData[i];
+          if(i !== 0 && this.interestTimeData[i-1] !== interest) {
+            annuity = this.calculateAnnuity(remaining, this.duration - i, interest);
+          }
+          const yearlyInterest = remaining * ( interest / 100 );
+          const repayment = annuity - yearlyInterest;
+          remaining -= repayment;
+        this.results.push({
+          yearlyInterest,
+          repayment,
+          remaining,
+          annuity
+        })
+      }
+    },
+    calculateAnnuity(amount, duration, interest) {
+      const realInterest = interest / 100;
+      const interestFactor = (1 + realInterest)**duration;
+      const inverseInterest = (interestFactor - 1);
+      const annuityFactor = ( interestFactor * realInterest ) / inverseInterest;
+      return annuityFactor * amount;
+    }
   }
 };
 </script>
 
 <style>
-.App {
-  text-align: center;
-}
-.App-header {
-  background-color: #f9f6f6;
-  color: #32485f;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-}
-.App-link {
-  color: #00c185;
-}
-.App-logo {
-  height: 40vmin;
-  pointer-events: none;
-  margin-bottom: 1rem;
-  animation: App-logo-spin infinite 1.6s ease-in-out alternate;
-}
-@keyframes App-logo-spin {
-  from {
-    transform: scale(1);
-  }
-  to {
-    transform: scale(1.06);
-  }
+th {
+  text-align: left;
 }
 </style>
---
+++ src/interestData.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    interest: 1.1,
+    duration: 35
+  },
+  {
+    interest: 1.1,
+    duration: 20,
+  },
+  {
+    interest: 1.1,
+    duration: 20,
+  }
+]
